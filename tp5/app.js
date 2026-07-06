f419983a85590d9bf32a2e381a9feb914d3b225b
alumno <52211088@ivt.edu.ar>
2026-07-06 14:31:10 -0300

    6/7/26

--- tp5/app.js
+++ tp5/app.js
@@ -2,3 +2,19 @@
 //declarar variable
 let numero1
 //Asignar valor
+
+//Variables
+
+let nombrealumnos
+
+console.log("suares jubete")
+console.log("maxi jubete")
+
+let numero1 = 100
+let numero2 = 50
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+
+
+
