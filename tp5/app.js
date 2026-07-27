c651d32ad3dbf3a9f35c45f562d45f415775db8e
alumno <52211088@ivt.edu.ar>
2026-07-27 14:29:42 -0300

    27/07/2026

--- tp5/app.js
+++ tp5/app.js
@@ -16,11 +16,23 @@ let numero2 = 50
 console.log(numero1 + numero2)
 console.log(numero1 - numero2)
 
-
-
 // 
 nombrealumnos = 'Benja Suarez Barrozo'
 
 let edad = 15
 
 console.log(nombrealumnos +' tiene ' + edad +' años de carcel') 
+
+let NombreMateria ='biologia'
+let NotaMateria = 5
+
+console.log(nombrealumnos + ' tiene un ' + NotaMateria + ' en ' + NombreMateria)
+
+//crear las siguientes variables
+
+let comidafavorita = 'lasaña'
+
+let colorfavorito = 'blanco'
+
+let nombredefamoso = 'bruno mars'
+
